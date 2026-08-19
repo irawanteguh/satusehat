c04d24c799aac384c5eb8b05cb7b865e167bae0a
Irawanteguh <teguhirawan.rsudpasarminggu@gmail.com>
2026-08-19 11:12:11 +0700

    update

--- nodejs/service.js
+++ nodejs/service.js
@@ -26,22 +26,20 @@ console.log(
 
 const services = [
     "patientid",
-    "poliklinik",
-    "anamnesaawalrj",
-    "hasillab",
-    "orderrad",
-    "orderlab",
-    "specimenlab",
-    "dicom",
-    "diaglaboratorium",
-    "careplan",
-    // "allergyintolerance",
-    "radiologi",
-    "cipoliklinik",
-    // "qpoliklinik",
-    "compoliklinik",
-    "singledose",
-    "singledosereq"
+    // "poliklinik",
+    // "anamnesaawalrj",
+    // "hasillab",
+    // "orderrad",
+    // "orderlab",
+    // "specimenlab",
+    // "dicom",
+    // "diaglaboratorium",
+    // "careplan",
+    // "radiologi",
+    // "cipoliklinik",
+    // "compoliklinik",
+    // "singledose",
+    // "singledosereq"
 ];
 
 
