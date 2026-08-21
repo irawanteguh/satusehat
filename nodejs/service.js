70e9c0e5816b64ad8f02358c9c6b7e5121f173fb
Irawanteguh <teguhirawan.rsudpasarminggu@gmail.com>
2026-08-21 15:27:23 +0700

    update

--- nodejs/service.js
+++ nodejs/service.js
@@ -93,11 +93,11 @@ function logHeader() {
 
     console.log(
         chalk.cyan(
-            "TIMESTAMP".padEnd(20) +
-            "METHOD".padEnd(7) +
-            "ENDPOINT".padEnd(32) +
-            "STATUS".padEnd(16) +
-            "MESSAGE"
+            "TIMESTAMP".padEnd(20)
+            +"METHOD".padEnd(7)
+            +"ENDPOINT".padEnd(32)
+            +"STATUS".padEnd(16)
+            // +"MESSAGE"
         )
     );
 
@@ -130,11 +130,11 @@ function logRow(
                 : chalk.green;
 
     console.log(
-        chalk.white(timestamp.padEnd(20)) +
-        chalk.white(method.padEnd(7)) +
-        chalk.white(endpoint.padEnd(32)) +
-        colorStatus(String(status).padEnd(16)) +
-        chalk.white(message)
+        chalk.white(timestamp.padEnd(20))
+         +chalk.white(method.padEnd(7))
+         +chalk.white(endpoint.padEnd(32))
+         +colorStatus(String(status).padEnd(16))
+        //  +chalk.white(message)
     );
 }
 
