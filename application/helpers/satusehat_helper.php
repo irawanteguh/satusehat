328d4543e58cc28a4e5d5ab9bc63315fd399e443
Irawanteguh <teguhirawan.rsudpasarminggu@gmail.com>
2026-08-21 15:40:19 +0700

    update

--- application/helpers/satusehat_helper.php
+++ application/helpers/satusehat_helper.php
@@ -13,9 +13,14 @@
     }
 
 
+    // function headerbundle(){
+    //     echo PHP_EOL;
+    //     echo color('cyan').str_pad("TIMESTAMP", WIDTH_TIMESTAMP).str_pad("PASIEN_ID", WIDTH_PASIEN_ID).str_pad("EPISODE_ID", WIDTH_NO_IDENTITAS).str_pad("RESOURCE_TYPE", WIDTH_RESOURCE_TYPE).str_pad("SATUSEHAT_ID", WIDTH_SATUSEHATID)."MESSAGE".PHP_EOL;
+    // }
+
     function headerbundle(){
         echo PHP_EOL;
-        echo color('cyan').str_pad("TIMESTAMP", WIDTH_TIMESTAMP).str_pad("PASIEN_ID", WIDTH_PASIEN_ID).str_pad("EPISODE_ID", WIDTH_NO_IDENTITAS).str_pad("RESOURCE_TYPE", WIDTH_RESOURCE_TYPE).str_pad("SATUSEHAT_ID", WIDTH_SATUSEHATID)."MESSAGE".PHP_EOL;
+        echo color('cyan').str_pad("TIMESTAMP", WIDTH_TIMESTAMP).str_pad("RESOURCE_TYPE", WIDTH_RESOURCE_TYPE).str_pad("SATUSEHAT_ID", WIDTH_SATUSEHATID)."MESSAGE".PHP_EOL;
     }
 
     function formatlogpasien($parameter1,$parameter2,$parameter3,$parameter4,$parameter5,$message,$colorIdentity='cyan'){
@@ -33,8 +38,8 @@
         $reset = color('reset');
 
         $formatted  = color($colorIdentity) . str_pad(date('Y-m-d H:i:s'), WIDTH_TIMESTAMP) . $reset;
-        $formatted .= color($colorIdentity) . str_pad($parameter1, WIDTH_PASIEN_ID) . $reset;
-        $formatted .= color($colorIdentity) . str_pad($parameter2, WIDTH_NO_IDENTITAS) . $reset;
+        // $formatted .= color($colorIdentity) . str_pad($parameter1, WIDTH_PASIEN_ID) . $reset;
+        // $formatted .= color($colorIdentity) . str_pad($parameter2, WIDTH_NO_IDENTITAS) . $reset;
         $formatted .= color($colorIdentity) . str_pad($parameter3, WIDTH_RESOURCE_TYPE) . $reset;
         $formatted .= color($colorIdentity) . str_pad($parameter4, WIDTH_SATUSEHATID) . $reset;
         $formatted .= color($colorIdentity) . $message . $reset;
